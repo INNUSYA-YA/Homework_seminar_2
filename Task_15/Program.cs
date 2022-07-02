f5f6b1a9ec23709fbe895b62f8a6d1701d3e9c48
Inna <innasilivonchik@mail.ru>
2022-07-02 14:01:44 +0300

    Решение задачи 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>=1 && number<=5)
+{
+    Console.WriteLine("День недели не является выходным");
+}
+if (number>=6 && number<=7)
+{
+    Console.WriteLine("День недели является выходным");
+}
