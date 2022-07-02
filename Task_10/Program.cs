eea95586138af4cb5fd12ae443c74d19e5f4761f
Inna <innasilivonchik@mail.ru>
2022-07-02 12:28:27 +0300

    Решение задачи 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int oneDigit = number/10;
+int secondDigit = oneDigit%10;
+Console.WriteLine($"Вторая цифра числа - {secondDigit}");
