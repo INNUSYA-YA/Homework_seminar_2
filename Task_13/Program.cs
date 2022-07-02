6ed94e1ba7ae5ac7578c2cf6439cdd9ebadbc3e7
Inna <innasilivonchik@mail.ru>
2022-07-02 13:48:39 +0300

    Решение задачи 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int threeNumber = 0;
+if(number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else //if(number>99 && number<1000)
+{
+    while (number>999)
+    {
+        number=number/10;
+    }
+threeNumber = number%10;
+Console.WriteLine(threeNumber);
+}
+
